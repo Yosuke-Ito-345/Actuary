3031483b1874be10544f290221b57a04d4a1cb19
amethyst <yosukeito@itouyousuke-no-MacBook-Air.local>
2021-07-23 16:29:09 +0900

    Newly added

---
+++ all_Actuary.v
@@ -0,0 +1,5 @@
+From Actuary Require Export Basics.
+From Actuary Require Export Interest.
+From Actuary Require Export LifeTable.
+From Actuary Require Export Premium.
+From Actuary Require Export Reserve.
